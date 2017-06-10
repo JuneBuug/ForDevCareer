81e9b5f692ea996f09ba79dbd8db84196402dc30
JuneBuug <junee613@gmail.com>
2017-06-10 20:50:27 +0900

    메인 탭, 앱 아이콘 추가

--- app/src/main/java/com/example/junekim/univinfo/MainActivity.java
+++ app/src/main/java/com/example/junekim/univinfo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.junekim.univinfo;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,13 +21,18 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.List;
+
 import static com.google.android.gms.internal.zzs.TAG;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity{
 
     @ViewById
-    TextView fb_test;
+    ListView main_list;
+
+    private ListViewAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +40,8 @@ public class MainActivity extends Activity{
 
     @AfterViews
     protected void afterViews(){
+
+        mAdapter = new ListViewAdapter();
 
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference myRef = db.getReference("message");
@@ -42,7 +54,6 @@ public class MainActivity extends Activity{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String value = dataSnapshot.getValue(String.class);
-                fb_test.setText(value);
                 Log.d(TAG,"Value is: " + value);
             }
 
@@ -51,5 +62,53 @@ public class MainActivity extends Activity{
                 Log.d(TAG,"값을 읽는데 실패했습니다.");
             }
         });
+    }
+
+
+
+    private class ListViewAdapter extends BaseAdapter {
+
+        private List<String> mList;
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            final ViewHolder holder;
+
+            if(convertView == null){
+                holder = new ViewHolder();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//                convertView = inflater.inflate
+
+                convertView.setTag(holder);
+            }else{
+                convertView.getTag();
+            }
+            return null;
+        }
+
+        private void setViewHolder(View convertView, ViewHolder holder) {
+
+        }
+
+    }
+
+    private class ViewHolder {
+        public TextView title, subtitle;
     }
 }

9286b5cc5778a520dd08bc59f46e98e45dcde78f
JuneBuug <junee613@gmail.com>
2017-06-30 22:18:25 +0900

    datepicker dialog 메테리얼로 추가해서 period추가

--- app/src/main/java/com/example/junekim/univinfo/More/SpecWriteActivity.java
+++ app/src/main/java/com/example/junekim/univinfo/More/SpecWriteActivity.java
@@ -1,9 +1,13 @@
 package com.example.junekim.univinfo.More;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
+import android.view.ContextThemeWrapper;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +26,7 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 
@@ -45,7 +50,41 @@ public class SpecWriteActivity extends Activity {
     @Click
     void period(){
         //TODO 달력 dialog 연결해서 시작 , 끝 날짜 입력 받고 개월 수 계산
+
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        final Context context = new ContextThemeWrapper(this, android.R.style.Theme_Material_Dialog);
+
+        final DatePickerDialog.OnDateSetListener seconddateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    period.setText(period.getText()+"~"+year+"."+(monthOfYear+1)+"."+dayOfMonth);
+            }
+        };
+
+
+        DatePickerDialog.OnDateSetListener firstdateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                period.setText(year+"."+(monthOfYear+1)+"."+dayOfMonth);
+
+                DatePickerDialog datePickerDialog2 = new DatePickerDialog(context, seconddateSetListener,  calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog2.show();
+            }
+        };
+
+
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context, firstdateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+
+
     }
+
+
 
 
     @Click
@@ -80,6 +119,7 @@ public class SpecWriteActivity extends Activity {
         String job = job_desc.getText().toString();
         String difficulty_point = difficulty.getText().toString();
         String learning_point = learning.getText().toString();
+        String period_str = period.getText().toString();
 
 
         if( spec_title.length() == 0 || spec_title == null || spec_title.isEmpty()){
@@ -87,6 +127,10 @@ public class SpecWriteActivity extends Activity {
             return;
         }
 
+        if( period_str.length() == 0 || period_str == null || period_str.isEmpty()){
+            Toast.makeText(this,"기간을 입력해주세요.",Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         if( job.length() == 0 || job == null || job.isEmpty()){
             Toast.makeText(this,"업무 내용을 입력해주세요.",Toast.LENGTH_SHORT).show();
@@ -106,7 +150,7 @@ public class SpecWriteActivity extends Activity {
             return;
         }
 
-        Spec spec = new Spec(spec_title,"4개월",job,difficulty_point,learning_point);
+        Spec spec = new Spec(spec_title,period_str,job,difficulty_point,learning_point);
 
         String uuid = UUID.randomUUID().toString();
         mDataReference.child("spec").child(uuid).setValue(spec);
